ba4f22002bada2c23f599f110af7ad0bcfe67e60
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2023-04-03 15:36:04 +0530

    delete

--- Module 10b - How JS Works/app1.js
+++
@@ -1,8 +0,0 @@
-const loginButton = document.querySelector(".login-btn");
-const modal = document.querySelector(".modal");
-
-modal.classList.add("hide");
-
-loginButton.addEventListener("click", () => {
-  modal.classList.add("show");
-});
