e854742f66e89b92e08a30650662adafc960f0c4
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2023-04-03 16:04:39 +0530

    delete

--- Module 10-1 - Projects/Project 5 - E-com Javacript/utils/findProductInCart.js
+++
@@ -1,5 +0,0 @@
-export const findProductInCart = (cart, prodId) => {
-  const isProductInCart =
-    cart && cart.length > 0 && cart.some(({ _id }) => _id === prodId);
-  return isProductInCart;
-};
